741b45d07c359cbccd97182c69a10953cbd57484
Ryan Tablada <ryan.tablada@gmail.com>
2016-08-23 23:11:28 -0500

    feat: User can login

---
+++ app/Http/Controllers/SessionController.js
@@ -0,0 +1,33 @@
+'use strict'
+
+class SessionController {
+
+  * create(request, response) {
+    yield response.sendView('session.create')
+  }
+
+  * store(request, response) {
+    const {email, password} = request.all()
+    try {
+      yield request.auth.attempt(email, password)
+    } catch (e) {
+      yield request
+        .withOut('password')
+        .andWith({
+          warning: 'Email and password do not match!'
+        }).flash()
+
+      return response.redirect('back')
+    }
+
+    yield request
+      .with({
+        warning: 'Email and password do not match!'
+      }).flash()
+
+    response.redirect('/app')
+  }
+
+}
+
+module.exports = SessionController
